edc85efe28d8a417d5c32df8b07535a83eccfa8c
BOOSTED <83159939+NotableAPP@users.noreply.github.com>
2021-04-29 12:31:14 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,23 +1,23 @@
 self.addEventListener('install', function(e) {
     e.waitUntil(
-      caches.open('global-web-chat').then(function(cache) {
+      caches.open('notableapp').then(function(cache) {
         return cache.addAll([
          
-          '/index.html',
-          '/app.js',
-          '/style.css',
-		  '/app data/',
-          '/app data/ico@max.png',
-		  '/app data/ico@192.png',
-		  '/app data/ico@384.png', 
-		  '/app data/ico@512.png',
-          '/app data/Mail.png',
-          '/app data/Sms.png',
-          '/app data/598px-WhatsApp.svg.png',
-          '/app data/1024px-Telegram_logo.svg.png',
-          '/cous.html',
-		  '/app data/note_add_black_24dp.svg', 	
-          '/app data/add_task_black_24dp.svg',
+          'index.html',
+          'app.js',
+          'style.css',
+		  'app data/',
+          'app data/ico@max.png',
+		  'app data/ico@192.png',
+		  'app data/ico@384.png', 
+		  'app data/ico@512.png',
+          'app data/Mail.png',
+          'app data/Sms.png',
+          'app data/598px-WhatsApp.svg.png',
+          'app data/1024px-Telegram_logo.svg.png',
+          'cous.html',
+		  'app data/note_add_black_24dp.svg', 	
+          'app data/add_task_black_24dp.svg',
 		  	
         ]);
       })
